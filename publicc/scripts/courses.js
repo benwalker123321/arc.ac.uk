ae9d0dea5dc6ac90fec7a8ba55a7aadc1c4a3684
benwalker123321 <ben1990sanjose@hotmail.co.uk>
2017-05-24 17:06:33 +0100

    Add files via upload

---
+++ publicc/scripts/courses.js
@@ -0,0 +1,39 @@
+
+    function viewViewCoursesForm(){
+        
+             document.getElementById('viewCoursesForm').style.display = 'inline',
+             document.getElementById('viewModulesForm').style.display = 'none', 
+             document.getElementById('viewCourseModulesForm').style.display = 'none',           
+             document.getElementById('viewModulesCoursesForm').style.display = 'none';
+             document.getElementById('message').style.display = 'none';
+            
+ }
+ 
+    function viewViewModulesForm(){         
+             document.getElementById('viewCoursesForm').style.display = 'none',
+             document.getElementById('viewModulesForm').style.display = 'inline', 
+             document.getElementById('viewCourseModulesForm').style.display = 'none',           
+             document.getElementById('viewModulesCoursesForm').style.display = 'none';
+             document.getElementById('message').style.display = 'none';            
+ }
+ 
+    function viewViewCourseModulesForm(){
+        
+             document.getElementById('viewCoursesForm').style.display = 'none',
+             document.getElementById('viewModulesForm').style.display = 'none', 
+             document.getElementById('viewCourseModulesForm').style.display = 'inline',           
+             document.getElementById('viewModulesCoursesForm').style.display = 'none';
+             document.getElementById('message').style.display = 'none';
+             
+ }
+ 
+    function viewViewModulesCoursesForm(){
+         
+             document.getElementById('viewCoursesForm').style.display = 'none',
+             document.getElementById('viewModulesForm').style.display = 'none', 
+             document.getElementById('viewCourseModulesForm').style.display = 'none',           
+             document.getElementById('viewModulesCoursesForm').style.display = 'inline';
+             document.getElementById('message').style.display = 'none';
+            
+ }
+
